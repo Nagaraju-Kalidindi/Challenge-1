bd521f918a97a5de4bfc7e29a3c0a1b13d261c5d
Nagaraju-Kalidindi <125673339+Nagaraju-Kalidindi@users.noreply.github.com>
2023-02-19 19:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.resource_group
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
   client_id       = var.client_id
