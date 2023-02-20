ce4dc69b1562074cd8592bdb5818c6da3888fd6d
Nagaraju-Kalidindi <125673339+Nagaraju-Kalidindi@users.noreply.github.com>
2023-02-20 12:48:08 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,16 +1,21 @@
-resource "azurerm_sql_server" "primary" {
-    name = var.primary_database
+resource "azurerm_mssql_server" "myprimarysql-server" {
+    name = "myprimarysql-server"
     resource_group_name = var.resource_group
     location = var.location
     version = var.primary_database_version
     administrator_login = var.primary_database_admin
-    administrator_login_password = var.primary_database_password
+    administrator_login_password = var.primary_database_password  
 }
 
-resource "azurerm_sql_database" "db" {
-  name                = "db"
+resource "azurerm_sql_database" "mydb" {
+  name                = "mydb"
   resource_group_name = var.resource_group
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"  
-  server_name         = azurerm_sql_server.primary.name
+  location            = var.location
+  server_name         = azurerm_mssql_server.myprimarysql-server.name
+
+  depends_on = [
+    azurerm_mssql_server.myprimarysql-server
+  ]
+
 }
+
